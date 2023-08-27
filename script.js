858e19c9d98e86e269b847dab584dce5c80810a7
Fathurrabbani <fath.ra001@gmail.com>
2023-08-28 04:59:14 +0700

    revisi

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ let engga = document.getElementById("engga");
 
 mau.addEventListener("click", (event) => {
   event.preventDefault();
-  window.location.href = "http://127.0.0.1:5500/lanjutan.html";
+  window.location.href = "https://eveqwe.github.io/Edelweiss/lanjutan.html";
 });
 
 engga.addEventListener("click", (event) => {
